4b6660e7e6f78dfbd91b4e3219613a91d55d4835
YeongKyunJeong <jjbykk9999@snu.ac.kr>
2026-08-05 19:07:40 +0900

    Push ch11 changed contents

--- ch11/widget2/option.py
+++ ch11/widget2/option.py
@@ -7,11 +7,11 @@ otk = Tk()
 otk.geometry("400x300")
 
 option_list = ['Option1', 'Option2', 'Option3']
-selceted_option = StringVar()
-selceted_option.set(option_list[0])
+selected_option = StringVar()
+selected_option.set(option_list[0])
 
 option_menu = OptionMenu(otk, 
-                         selceted_option, 
+                         selected_option, 
                          *option_list)
 option_menu.pack()
 
